6ffcfa66a6121823c692e76a503cfd0fe1da18be
FumiyaAdachi <adachi@launchcraft.jp>
2023-05-22 14:50:18 +0900

    add toggle buttons

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
-class FirstPage extends StatelessWidget {
+class FirstPage extends StatefulWidget {
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
   final List<String> entries = <String>['きなこ', 'もな'];
   final List<String> imagePaths = <String>['kina', 'mona'];
   final double imageSize = 120;
-  FirstPage({super.key});
+  final mornigFlag = [false, false];
+  final dinnerFlag = [false, false];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Confirmation Food',
+        title: const Text(
+          'Confirmation Food',
           style: TextStyle(
-            fontWeight: FontWeight.bold
+            fontWeight: FontWeight.bold,
           ),
         ),
         backgroundColor: Colors.red[200],
@@ -45,34 +52,65 @@ class FirstPage extends StatelessWidget {
                       shape: BoxShape.circle,
                       image: DecorationImage(
                         fit: BoxFit.fill,
-                        image: AssetImage("images/${imagePaths[index]}.jpg")
-                      )
+                        image: AssetImage("images/${imagePaths[index]}.jpg"),
+                      ),
                     ),
                   ),
                   const SizedBox(width: 10),
                   Row(
                     children: [
-                      Container(
-                        height: imageSize,
-                        width: imageSize,
-                        color: Colors.white,
-                        child: const Icon(Icons.light_mode, size: 50,),
+                      // Container(
+                      //   height: imageSize,
+                      //   width: imageSize,
+                      //   color: Colors.white,
+                      //   child: const Icon(Icons.light_mode, size: 50,),
+                      // ),
+                      // const SizedBox(width: 10),
+                      // Container(
+                      //   height: imageSize,
+                      //   width: imageSize,
+                      //   color: Colors.white,
+                      //   child: const Icon(Icons.dark_mode, size: 50,),
+                      // ),
+                      SizedBox(
+                        width: 120,
+                        height: 120,
+                        child: ElevatedButton.icon(
+                          onPressed: () => setState(() => 
+                            mornigFlag[index] = !mornigFlag[index]
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: mornigFlag[index] ? Colors.red[200] : Colors.blue,
+                          ),
+                          icon: Icon(Icons.light_mode),
+                          label: mornigFlag[index] ? Text('おなかいっぱいだよ') : Text('おなかすいたよ')
+                        ),
                       ),
-                      const SizedBox(width: 10),
-                      Container(
-                        height: imageSize,
-                        width: imageSize,
-                        color: Colors.white,
-                        child: const Icon(Icons.dark_mode, size: 50,),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: 120,
+                        height: 120,
+                        child: ElevatedButton.icon(
+                          onPressed: () => setState(() => 
+                            dinnerFlag[index] = !dinnerFlag[index]
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: dinnerFlag[index] ? Colors.red[200] : Colors.blue,
+                          ),
+                          icon: Icon(Icons.dark_mode),
+                          label: dinnerFlag[index] ? Text('おなかいっぱいだよ') : Text('おなかすいたよ')
+                        ),
                       ),
                     ],
-                  )
+                  ),
                 ],
               ),
             ),
           );
-        }
-      )
+        },
+      ),
     );
   }
 }
