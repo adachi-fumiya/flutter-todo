2587fbe23cf52d6dabaa2a4b9457f11b0bad3f39
FumiyaAdachi <adachi@launchcraft.jp>
2023-05-22 13:53:35 +0900

    一覧を作成

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,43 +1,77 @@
 import 'package:flutter/material.dart';
-import 'second_page.dart';
 
 class FirstPage extends StatelessWidget {
-  String nameText = '';
-
+  final List<String> entries = <String>['きなこ', 'もな'];
+  final List<String> imagePaths = <String>['kina', 'mona'];
+  final double imageSize = 120;
   FirstPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('first'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder()
-                ),
-                onChanged: (text) {
-                  print(text);
-                  nameText = text;
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SecondPage(nameText))
-                  );
-                },
-                child: const Text('hoge'),
-              ),
-            ],
+        title: const Text('Confirmation Food',
+          style: TextStyle(
+            fontWeight: FontWeight.bold
           ),
-        )
+        ),
+        backgroundColor: Colors.red[200],
+        actions: const [
+          // Icon(Icons.cast),
+          // SizedBox(width: 24),
+          // Icon(Icons.search),
+          // SizedBox(width: 24),
+          // Icon(Icons.bar_chart),
+        ],
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: entries.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 150,
+            // width: double.infinity,
+            // padding: const EdgeInsets.all(8),
+            child: Container(
+              // color: Colors.orange,
+              child: Row(
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // プロフィール画像
+                  Container(
+                    width: 110.0,
+                    height: 110.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage("images/${imagePaths[index]}.jpg")
+                      )
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Row(
+                    children: [
+                      Container(
+                        height: imageSize,
+                        width: imageSize,
+                        color: Colors.white,
+                        child: const Icon(Icons.light_mode, size: 50,),
+                      ),
+                      const SizedBox(width: 10),
+                      Container(
+                        height: imageSize,
+                        width: imageSize,
+                        color: Colors.white,
+                        child: const Icon(Icons.dark_mode, size: 50,),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        }
       )
     );
   }
